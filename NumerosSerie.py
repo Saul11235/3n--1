c082c302727f77898fd478a453d7f0cbf99d406e
Edwin Saul <edwinsaulpm@gmail.com>
2021-12-12 22:10:45 -0500

    elaborando script

---
+++ NumerosSerie.py
@@ -0,0 +1,45 @@
+#script que calcula todos los numero de la conjetura de collatz
+#No pierdas tiempo con esto anda a hacer otras cosas  :(
+
+def NumerosConjetura(numeroEntrada):
+    """Devuelve una lista con 3 elementos.\n0- una lista con todos los numeros\n1- numero de elementos\n2- numero maximo de la lista\n3- posicion del numero maximo"""
+    numeroEntrada=int(abs(numeroEntrada))
+    if not(numeroEntrada): numeroEntrada=1 #eliminando valores no fraccion ni negativos 
+    numeros=[] #lista de nros
+    numerodeElementos=0
+    numeroMaximo=numeroEntrada
+    posicionNumeroMaximo=0
+    numeroDePrueba=numeroEntrada #numero que se calculara
+    calcularvalores=True 
+    UltimoCiclo=False
+    while calcularvalores:
+        #analizando si el numeroDePrueba es par
+        espar=not(bool(numeroDePrueba/2-int(numeroDePrueba/2)))
+        numerodeElementos=numerodeElementos+1
+        if espar:
+            numeroDePrueba=int(numeroDePrueba/2)
+        else:
+            numeroDePrueba=int(numeroDePrueba*3+1)
+            if numeroMaximo<numeroDePrueba:
+                numeroMaximo=numeroDePrueba
+                posicionNumeroMaximo=numerodeElementos
+        numeros.append(numeroDePrueba)    
+        if numeroDePrueba==1:
+            calcularvalores=False #fin del calculo
+    return([numeros,numerodeElementos,numeroMaximo,posicionNumeroMaximo])
+    
+
+if __name__=="__main__":
+    print("Conjetura de collatz")
+    numero=abs(int(input(" >>> ")))
+    lista=NumerosConjetura(numero)
+    print(" Numero de variables ciclo : "+str(lista[1]))
+    print(" Numero Maximo : "+str(lista[2]))
+    print(" Posicion Numero Maximo : "+str(lista[3]))
+    input("> presione enter para continuar ")
+    contador=0
+    for x in lista[0]:
+        contador=contador+1
+        print(str(contador)+"\t"+str(x))
+ 
+ 
